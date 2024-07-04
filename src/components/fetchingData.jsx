c72132d90ff20ab5b773ed1bf6c104f7f59b66b3
Dark Wood <anonymoushackerintheweb@gmail.com>
2024-07-04 18:51:45 +0545

    Adding the api feature while fetching the pokemon images

--- src/components/fetchingData.jsx
+++ src/components/fetchingData.jsx
@@ -1,27 +1,48 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-import { v4 as uuid4} from 'uuid'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { v4 as uuid4 } from "uuid";
 
 function Fetcher(props) {
-    const [images, setImages] = useState({})
-    const [pokemons, setPokemon] = useState(
-        [
-            {pokeName: "pikachu", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg"},
-            {pokeName: "alakazam", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/65.svg"},
-            {pokeName: "alomomola", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/594.svg"},
-            {pokeName: "bayleef", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/153.svg"},
-            {pokeName: "cacturne", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/332.svg"},
-            {pokeName: "haunter", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/93.svg"},
-            {pokeName: "exeggutor", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/103.svg"},
-            {pokeName: "electabuzz", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/125.svg"},
-            {pokeName: "psyduck", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/54.svg"},
-            {pokeName: "cascoon", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/268.svg"},
-            {pokeName: "gengar", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/94.svg"},
-            {pokeName: "zapdos", id: uuid4(), img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/145.svg"},
-        ]
-    )
-    const [tempArray, setTempArray] = useState([])
-    const [randomizedArray, setRandomizedArray] = useState([...pokemons])
+    const [pokemonNames, setPokemonNames] = useState([
+        "pikachu",
+        "alakazam",
+        "alomomola",
+        "bayleef",
+        "cacturne",
+        "haunter",
+        "exeggutor",
+        "mewtwo",
+        "psyduck",
+        "cascoon",
+        "gengar",
+        "zapdos",
+    ]);
+    const [pokemons, setPokemon] = useState([]);
+    const [tempArray, setTempArray] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const requests = pokemonNames.map(async (pokemon) => {
+                    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+                    return {
+                        id: uuid4(),
+                        pokeName: pokemon,
+                        img: response.data.sprites.front_default,
+                    };
+                });
+    
+                const newPokemonData = await Promise.all(requests);
+                setPokemon(newPokemonData);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+    
+        if (pokemonNames.length > 0) {
+            fetchData();
+        }
+    }, [pokemonNames]);
 
     const createArray = (pok) => {
         let alreadyExists = false
@@ -43,11 +64,12 @@ function Fetcher(props) {
     }
 
     const arrange = () => {
-        const shuffledArray = [...randomizedArray];
+        const shuffledArray = [...pokemons];
         for (let i = shuffledArray.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
         }
+        console.log(shuffledArray)
         setPokemon(shuffledArray);
     }
 
@@ -56,13 +78,13 @@ function Fetcher(props) {
             {pokemons.map((poke) => {
                 return (
                     <div className="cell" key={poke.id} name={poke.pokeName} onClick={() => createArray(poke.pokeName)}>
-                        <img src={poke.img} alt="" className="pokemon-images"/>
+                        <img src={poke.img} alt="" className="pokemon-images" />
                         <h2 className="pokemon-names">{poke.pokeName}</h2>
                     </div>
-                )
+                );
             })}
         </div>
-    )
+    );
 }
 
-export default Fetcher 
+export default Fetcher;
