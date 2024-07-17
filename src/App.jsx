fe397f4be5d65b63d83f790720671a7d3ba173d9
Dark Wood <anonymoushackerintheweb@gmail.com>
2024-07-17 21:01:04 +0545

    Completed the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import Heading from './components/header'
 import './App.css'
 import Fetcher from './components/fetchingData'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [score, setScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
   const [resetCalled, setResetCalled] = useState(false)
   const [prevScore, setPrevScore] = useState(null)
+
+  const [levels, setLevels] = useState({
+    "1": { score: 0, completed: false, endScore: 12 },
+    "2": { score: 0, completed: false, endScore: 18 },
+    "3": { score: 0, completed: false, endScore: 18 }
+  })
+
+  const [currentLevel, setCurrentLevel] = useState("1")
 
   const increment = () => {
     endStage()
@@ -26,6 +34,26 @@ function App() {
     setResetCalled(true)
   }
 
+  const changeStage = () => {
+    setLevels({...levels, [currentLevel]: {score, completed: true}})
+    setScore(0)
+    setPrevScore(0)
+    setBestScore(0)
+    setCurrentLevel((Number(currentLevel) + 1).toString())
+  }
+
+  const resetGame = () => {
+    setLevels({
+      "1": { score: 0, completed: false, endScore: 12 },
+      "2": { score: 0, completed: false, endScore: 18 },
+      "3": { score: 0, completed: false, endScore: 18 }
+    })
+    setCurrentLevel("1")
+    setScore(0)
+    setPrevScore(0)
+    setBestScore(0)
+  }
+
   if (resetCalled) {
     return (
       <div className="screen">
@@ -36,20 +64,34 @@ function App() {
       </div>
     )
   }
+  if ((currentLevel === "3") && (score === levels[currentLevel].endScore)) {
+    return (
+      <div className="screen">
+        <h1>You Completed The Game!! 🎉</h1>
+        <button onClick={resetGame} className='play-again'>Play Again?</button>
+      </div>
+    )
+  } else if (score === levels[currentLevel].endScore) {
 
-  if (score === 12) {
     return (
       <div className="screen">
         <h1>You Won!</h1>
-        <button onClick={() => setScore(0)} className='play-again'>Play Again</button>
+        <button onClick={() => setScore(0)} className='play-again'>Play Again?</button>
+        {levels["3"].completed ? (
+          <h1>You completed the game, congrats!!</h1>
+        ) : (
+          <button onClick={changeStage} className='advance'>Advance to Level {Number(currentLevel) + 1}?</button>
+
+        )}
       </div>
     )
   }
 
+
   return (
     <div id='all-elements'>
-      <Heading bestPoints={bestScore} points={score}/>
-      <Fetcher increment={increment} reset={reset} />
+      <Heading bestPoints={bestScore} points={score} level={currentLevel}/>
+      <Fetcher increment={increment} reset={reset} level={currentLevel} />
     </div>
   )
 }
